a602a25b8de50adc7bf6e9a450b1131dd15eb516
Vadim Shalamov <reetvvy@gmail.com>
2024-01-24 01:04:35 +0200

    lesson-11 task-1 start

---
+++ src/main/java/de/aittr/g_27_shop_demo/G27ShopDemoApplication.java
@@ -0,0 +1,13 @@
+package de.aittr.g_27_shop_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class G27ShopDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(G27ShopDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/aittr/g_27_shop_demo/G27ShopDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package de.aittr.g_27_shop_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class G27ShopDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
